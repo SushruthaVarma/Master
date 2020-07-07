6d33d8cd2771ad2c5efdbf0f181366867576b179
Ahmed Fathi <ahmedfathi680@gmail.com>
2020-07-07 19:42:04 +0200

    Initial Commit

---
+++ AnyDoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnyDoc
+//
+//  Created by Ahmed Fathi on 7/7/20.
+//  Copyright © 2020 Ahmed Fathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
